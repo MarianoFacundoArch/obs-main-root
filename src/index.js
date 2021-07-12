e1582cb70b055d7d5a4907f63858f2c7233e24d0
Facundo <facundo@admincentralmail.com>
2021-07-12 16:23:25 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const dns2 = require('dns2');
+const { Packet } = dns2;
+
+const server = dns2.createServer({
+    udp: true,
+    handle: (request, send, rinfo) => {
+        const response = Packet.createResponseFromRequest(request);
+        const [ question ] = request.questions;
+        const { name } = question;
+        console.log(name)
+        response.answers.push({
+            name,
+            type: Packet.TYPE.A,
+            class: Packet.CLASS.IN,
+            ttl: 30,
+            address: '8.8.8.8'
+        });
+
+        response.answers.push({
+            name,
+            type: Packet.TYPE.A,
+            class: Packet.CLASS.IN,
+            ttl: 30,
+            address: '8.8.8.4'
+        });
+        send(response);
+    }
+});
+
+server.on('request', (request, response, rinfo) => {
+    console.log(request.header.id, request.questions[0]);
+});
+
+server.on('listening', () => {
+    console.log('Listenning');
+});
+
+server.on('close', () => {
+    console.log('server closed');
+});
+
+server.listen({
+    udp: 5333
+});
+
